39f035b38b6cc7b9dd97895d522d007d3a6ac4fc
tp041799 <66407121+tp041799@users.noreply.github.com>
2020-06-10 12:26:56 +0800

    Update insertion.php

--- insertion.php
+++ insertion.php
@@ -1,1 +1,24 @@
+<?php
 
+$connectionInfo = array("UID" => "apuadmin", "pwd" => "Akirazone398739", "Database" => "MY_DB_1", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:webappfordbtp041799.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+ if (!$conn)
+ {
+ die("Error connection: ".sqlsrv_errors());
+ }
+
+ echo "Yeahh connected";
+
+ $sql = "INSERT INTO restaurant (restaurant_name, restaurant_address, restaurant_phone) VALUES (?, ?, ?)";
+ $params = array("Burger King", "Bukit Jalil", "03-11223444");
+
+ 
+
+ $stmt = sqlsrv_query( $conn, $sql, $params);
+ if( $stmt === false ) {
+        die( print_r( sqlsrv_errors(), true));
+ }
+
+?>
