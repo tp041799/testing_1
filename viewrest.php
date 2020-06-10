5cfa4c5401399420ad3723a0eab84906a6cab219
tp041799 <66407121+tp041799@users.noreply.github.com>
2020-06-10 12:20:39 +0800

    Update viewrest.php

--- viewrest.php
+++ viewrest.php
@@ -5,12 +5,34 @@ $connectionInfo = array("UID" => "apuadmin", "pwd" => "Akirazone398739", "Databa
 $serverName = "tcp:webappfordbtp041799.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 
-if (!$conn)
-{
+ if (!$conn)
+ {
  die("Error connection: ".sqlsrv_errors());
  }
 
  echo "Yeahh connected";
 
+ $tsql= "SELECT * FROM [dbo].[restaurant]";
+ $getResults= sqlsrv_query($conn, $tsql);
 
+ if ($getResults == FALSE)
+ {
+ die(sqlsrv_errors());
+ }
+
+ echo "<table border = '1'>";
+
+ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC))
+{
+ echo "<tr>";
+ echo "<td>". $row['restaurant_id'] . "</td>";
+ echo "<td>". $row['restaurant_name'] ."</td>";
+ echo "<td>". $row['restaurant_address'] . "</td>";
+ echo "<td>". $row['restaurant_phone'] . "</td>";
+ echo "</tr>";
+ }
+ echo "</table>";
+
+
+ sqlsrv_free_stmt($getResults);
 ?>
